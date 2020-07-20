50c346fa844a5282063600e12462bce62cf53e56
NicholasS13 <nksahu@verizon.net>
2020-07-20 17:02:09 -0400

    started 2nd contribution section

--- js/index.js
+++ js/index.js
@@ -3,9 +3,11 @@ function subtract(){
     value = parseInt(document.getElementById("money").innerHTML);
     value-=1;
     document.getElementById("money").innerHTML = value; 
+    document.getElementById("money2").innerHTML = value;
 }
 function addition(){
     value = parseInt(document.getElementById("money").innerHTML);
     value+=1;
     document.getElementById("money").innerHTML = value; 
-} 
+    document.getElementById("money2").innerHTML = value;
+}
