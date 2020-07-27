ee0cef45d29e13be652fef56b75594198f5fc9ef
NicholasS13 <nksahu@verizon.net>
2020-07-27 17:12:09 -0400

    updated Ventilator distribution and started working on bugs

--- js/index.js
+++ js/index.js
@@ -3,11 +3,18 @@ function subtract(){
     value = parseInt(document.getElementById("money").innerHTML);
     value-=1;
     document.getElementById("money").innerHTML = value; 
-    document.getElementById("money2").innerHTML = value;
 }
 function addition(){
     value = parseInt(document.getElementById("money").innerHTML);
     value+=1;
     document.getElementById("money").innerHTML = value; 
-    document.getElementById("money2").innerHTML = value;
+
 }
+printScreen();
+function printScreen(){
+    console.log(window.innerWidth+"px ->width");
+    console.log(window.innerHeight+"px ->height");
+}
+
+
+//1169 is max width for mobile device 
